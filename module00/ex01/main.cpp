47cdf61fb4275d76246ff9e3c322a75e89a4818c
cwenz <wenzconrad@gmail.com>
2024-02-09 16:50:31 +0100

    added main and makefile

---
+++ module00/ex01/main.cpp
@@ -0,0 +1,32 @@
+#include "Contact.hpp"
+#include "Phonebook.hpp"
+
+#include <iostream>
+#include <string> // For std::string
+
+int main()
+{
+	Phonebook phonebook;
+	std::string userInput;
+
+	phonebook.startMessage();
+	while (true)
+	{
+		std::cout << "Command: ";
+		if (!std::getline(std::cin, userInput))
+			break ;
+		if (userInput.compare("SEARCH") == 0) {
+			phonebook.search();
+		}
+		else if (userInput.compare("ADD") == 0) {
+			Contact newContact;
+			newContact.inputContactDetails();
+			phonebook.addContact(newContact);
+		}
+		else if (userInput.compare("EXIT") == 0)
+			break ;
+		else
+			std::cout << "Invalid command, try again stupid." << std::endl;
+	}
+	return (0);
+}

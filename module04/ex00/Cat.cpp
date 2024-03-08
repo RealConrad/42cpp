cda3d587f7a68ea8115a8e07962d61ace94910fd
cwenz <wenzconrad@gmail.com>
2024-03-08 15:05:08 +0100

    added orthodox cononcal form

--- module04/ex00/Cat.cpp
+++ module04/ex00/Cat.cpp
@@ -5,6 +5,18 @@ Cat::Cat() {
 	std::cout << "Cat constructor called" << std::endl;
 }
 
+Cat::Cat(const Cat& other): Animal(other) {
+	std::cout << "Cat copy constructor called" << std::endl;
+}
+
+Cat& Cat::operator=(const Cat& other) {
+	if (this != &other) {
+		Animal::operator=(other);
+		std::cout << "Cat copy assignment called" << std::endl;
+	}
+	return *this;
+}
+
 Cat::~Cat() {
 	std::cout << "Cat desconstructor called" << std::endl;
 }
--- module04/ex00/Dog.cpp
+++ module04/ex00/Dog.cpp
@@ -5,6 +5,18 @@ Dog::Dog() {
 	std::cout << "Dog constructor called" << std::endl;
 }
 
+Dog::Dog(const Dog& other): Animal(other) {
+	std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& other) {
+	if (this != &other) {
+		Animal::operator=(other);
+		std::cout << "Dog copy assignment called" << std::endl;
+	}
+	return *this;
+}
+
 Dog::~Dog() {
 	std::cout << "Dog desconstructor called" << std::endl;
 }

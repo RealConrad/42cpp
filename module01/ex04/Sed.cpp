390d3c9869100d16c2041288d5d6eef9f5c088e2
Conrad <wenzconrad@gmail.com>
2024-03-11 18:09:19 +0000

    added fix for forbidden function

--- module01/ex04/Sed.cpp
+++ module01/ex04/Sed.cpp
@@ -33,16 +33,27 @@ void Sed::replace(void) {
 	}
 }
 
-static std::string replaceAll(std::string line, const std::string &s1, const std::string &s2) {
-	if (s1.empty())
-		return (line);
-	size_t startPosition = 0;
-	while (true) {
-		// If find() does not find s1 in the string, it returns `std::string::npos` a special constant (no match found)
-		if ((startPosition = line.find(s1, startPosition)) == std::string::npos)
-			break ;
-		line.replace(startPosition, s1.length(), s2);
-		startPosition += s2.length();
-	}
-	return (line);
+static std::string replaceAll(std::string str, const std::string& s1, const std::string& s2) {
+    // nothing to replace so just return
+	if (s1.empty()) 
+		return str;
+
+    std::string result;
+    size_t startPosition = 0;
+    size_t foundPosition = str.find(s1, startPosition);
+
+	// If find() does not find s1 in the string, it returns `std::string::npos` a special constant (no match found)
+	// then we break out
+    while (foundPosition != std::string::npos) {
+        // Append the part of the string before the found substring
+        result += str.substr(startPosition, foundPosition - startPosition);
+        result += s2;
+        // update pointer
+        startPosition = foundPosition + s1.length();
+        // Look for the s1;
+        foundPosition = str.find(s1, startPosition);
+    }
+    // Append any remaining parts of the original string after the last replacement
+    result += str.substr(startPosition);
+    return result;
 }

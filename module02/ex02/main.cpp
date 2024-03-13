c92c19d087e3db80c4f80a6c857c71aba1f56823
cwenz <wenzconrad@gmail.com>
2024-03-13 15:59:24 +0100

    added comment

--- module02/ex02/main.cpp
+++ module02/ex02/main.cpp
@@ -4,6 +4,15 @@ int main( void ) {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	std::cout << a << std::endl;
+	/*
+	Reason for small increase:
+		- When you increment the internal fixedPoint value by 1, 
+			it represents a very small increment in the actual floating-point value because of the scaling factor.
+			This is correct for fixed-point arithmetic with a high precision (many fractional bits).
+		- The scaling factor determined by fractionalBits, when converting back to a float for output. 
+			the increment appears as a very small change. 
+		- (e.g.,  fractionalBits = 8, then 1 in fixed-point representation is 1 / 2^8 = 0.00390625 in float)
+	*/
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;

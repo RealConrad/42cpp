0dcd426d8d5f3d269d087d978a2166ccbbb91319
cwenz <wenzconrad@gmail.com>
2024-03-19 11:37:29 +0100

    added module07/ex00

---
+++ module07/ex00/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "whatever.hpp"
+
+int main() {
+	int a = 2;
+	int b = 3;
+
+	std::cout << "==========TEST 1==========" << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
+	std::cout << "---SWAPPED VALUES!---" << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	
+
+	std::cout << "\n==========TEST 2==========" << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	
+	
+	std::cout << "---SWAPPED VALUES!---" << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+}

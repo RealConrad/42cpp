25fb3caeac2f6add4e96e34b381d8af03807ff7f
cwenz <wenzconrad@gmail.com>
2024-03-26 16:04:58 +0100

    added basic files

---
+++ module09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,15 @@
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+class BitcoinExchange {
+	private:
+	
+
+	public:
+		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange& other);
+		BitcoinExchange& operator=(const BitcoinExchange& other);
+		~BitcoinExchange();
+};
+
+#endif
